eb06774039d0fbc210ce120ae627c907f5058311
Kohe Tokoi <tokoik@gmail.com>
2026-05-16 20:19:45 +0900

    update main.cpp

--- main.cpp
+++ main.cpp
@@ -335,8 +335,8 @@ static void mouse(int button, int state, int x, int y)
       break;
     }
     break;
-    default:
-      break;
+  default:
+    break;
   }
 }
 
@@ -362,7 +362,7 @@ static void keyboard(unsigned char key, int x, int y)
 /*
 ** メインプログラム
 */
-int main(int argc, char *argv[])
+int main(int argc, char* argv[])
 {
   glutInit(&argc, argv);
   glutInitDisplayMode(GLUT_RGBA | GLUT_DEPTH | GLUT_DOUBLE);
